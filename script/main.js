58aced189205451f0c33899fe84f516d94b85617
rokan45 <rokanuzzaman4567@gmail.com>
2026-03-07 04:17:49 +0600

    showing status using icon in issue card that it is open or closed by conditional rendering

--- script/main.js
+++ script/main.js
@@ -5,7 +5,17 @@ const loadIssueData = () => {
     console.log(url);
     fetch(url)
         .then((res) => res.json())//promise
-        .then(json => displayIssueData(json.data))
+        .then(json => {
+
+            displayIssueData(json.data)
+        })
+}
+
+const createElements = (arr) => {
+    const htmlElements = arr.map((el) =>
+        `<button class="btn btn-soft">${el}</button>`)
+
+    return htmlElements.join(" ");
 }
 //display the data
 const displayIssueData = (issues) => {
@@ -20,15 +30,14 @@ const displayIssueData = (issues) => {
         const issueCard = document.createElement("div");
         issueCard.innerHTML = ` <div class="p-4 rounded-sm shadow-sm w-full max-w-sm">
             <div class="flex justify-between items-center mb-3">
-                <img src="./images/Open-Status.png" alt="" class="w-6 h-6 object-contain">
+                <div>${issue.status === "open" ? `<img src="./images/Open-Status.png" alt="" class="w-6 h-6 object-contain"></img>` : `<img src="./images/Closed-Status.png" alt="" class="w-6 h-6 object-contain"></img>`}
+                </div>
                 <button class="btn rounded-4xl">${issue.priority}</button>
             </div>
             <h2 class="text-sm font-semibold mb-2">${issue.title}</h2>
             <p class="text-sm text-[#64748B] text-wrap">${issue.description}</p>
 
-            <div class="felx flex-wrap items-center mt-3 space-x-4">
-                <button class="btn btn-soft">Bug</button>
-                <button class="btn btn-soft">Bug</button>
+            <div class="felx flex-wrap items-center mt-3 space-x-2">  ${createElements(issue.labels)}
             </div>
             <hr class="border-[#E4E4E7] mt-2">
             <div class="p-4 space-y-2">
@@ -43,4 +52,5 @@ const displayIssueData = (issues) => {
 
     });
 }
+
 loadIssueData();
